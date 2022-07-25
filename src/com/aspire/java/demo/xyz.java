b112163d08524fcdf51640967e81b4e148d90976
Admin <Admin@DESKTOP-T80N3AO>
2022-07-25 16:51:21 +0530

    3rd commit

--- src/com/aspire/java/demo/xyz.java
+++ src/com/aspire/java/demo/xyz.java
@@ -2,7 +2,14 @@ package com.aspire.java.demo;
 
 public class xyz 
 {
- public static void main(String[] args) {
-	
+ public static void main(String[] args)
+ {
+ System.out.println(" hi");
+ int a=10;
+ int b=20;
+ int sum= a+b;
+ {
+	 System.out.println(sum);
+ }
 }
 }
